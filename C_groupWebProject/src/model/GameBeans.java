efae7aba62dac3f035254c7eac38c9e5727348fd
konkon04 <konkon04@users.noreply.github.com>
2019-10-30 21:51:39 +0900

    Beans作っときました

--- C_groupWebProject/src/model/GameBeans.java
+++ C_groupWebProject/src/model/GameBeans.java
@@ -2,8 +2,51 @@ package model;
 
 import java.io.Serializable;
 
+/**
+ * @author 近藤
+ *
+ * ユーザアカウントを保存するJavaBeans<br>
+ * プロパティはname, password
+ */
 public class GameBeans
 implements Serializable
 {
+
+	/** アカウントID名 */
+	private String game;
+
+	/** アカウントパスワード */
+	private String date;
+
+	/** アカウントパスワード */
+	private String score;
+
+	/*
+	 * ------------------Accessor----------------------
+	 */
+
+	/** JavaBeans Accessor */
+	public String getGame()
+	{ return this.game; }
+
+	/** JavaBeans Accessor */
+	public void setGame(String game)
+	{ this.game = game; }
+
+	/** JavaBeans Accessor */
+	public String getDate()
+	{ return this.date; }
+
+	/** JavaBeans Accessor */
+	public void setDate(String date)
+	{ this.date = date; }
+
+	/** JavaBeans Accessor */
+	public String getScore()
+	{ return this.score; }
+
+	/** JavaBeans Accessor */
+	public void setScore(String score)
+	{ this.score = score; }
 
 }
