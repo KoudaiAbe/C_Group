cf69a25172c7be15a1751ea60a18c2e37a282a69
saena <saena@192.168.0.13>
2019-11-05 18:25:13 +0900

    少しだけ調整

--- C_groupWebProject/src/servlet/Login.java
+++ C_groupWebProject/src/servlet/Login.java
@@ -1,3 +1,11 @@
+
+/* **CHANGED**
+ *
+ * param(accounBeans) -> account
+ *
+ * column99-109 add
+ */
+
 package servlet;
 
 import java.io.IOException;
@@ -15,103 +23,123 @@ import dao.AccountRegistDAO;
 import model.AccountBeans;
 
 /**
- * Servlet implementation class Login
+ * アカウントを取り扱うサーブレット。<br>
+ * ログインの処理とアカウント作成の処理をそれぞれのメソッドで行う。<br>
+ * login.jspのフォームより値を受け取り、ログインであればdoGet()、アカウント作成であればdoPost()を実行する。
+ *
+ * @author 梅本/大河内
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public Login() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
+	 * ログインの処理を行うメソッド。
+	 *
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-
-	/** ログイン処理 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 
 		//Login.jspのNameとPassを受け取る。
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
 
 		//nameとpassの値をaccounBeansに保存。
-		AccountBeans accounBeans = new AccountBeans();
-		accounBeans.setName(name);
-		accounBeans.setPass(pass);
+		AccountBeans account = new AccountBeans();
+		account.setName(name);
+		account.setPass(pass);
 		HttpSession session = request.getSession();
-		session.setAttribute("accounBeans",accounBeans);
+		session.setAttribute("accounBeans",account);
 
+		// セッションスコープへ処理内容を送信
+		String action = "get";
+		session.setAttribute("action", action);
 
 		//AccountDAO生成
 		AccountDAO  accountDAO = new AccountDAO();
 
 		//既存アカウントと新規アカウントの精査
-		boolean check =accountDAO.getAccount(accounBeans);
+		boolean check =accountDAO.getAccount(account);
 
 		if(check) {
 
-			//既存：ログイン完了。
-			// フォワード
-			RequestDispatcher dispatcher =
-					request.getRequestDispatcher("/loginResult.jsp");
+			//ログイン完了
+			// loginResult.jspへ
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/loginResult.jsp");
 			dispatcher.forward(request, response);
-			System.out.println(2);
 
 		}else {
 
-			//ログイン失敗、リダイレクト。
+			//ログイン失敗
+			// セッションスコープとBeansを破棄
 			session.removeAttribute("accounBeans");
-			accounBeans = null;
-			RequestDispatcher dispatcher =
-					request.getRequestDispatcher("/login.jsp");
+			account = null;
+
+			// login.jspへ
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
 			dispatcher.forward(request, response);
 
 		}
 	}
 
 
-	/** アカウント作成処理 */
+
+	/**
+	 * アカウント作成処理を行うメソッド。
+	 *
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-System.out.println("post");
+
 		//LoginページのNameとPassを受け取る。
 		String name = request.getParameter("name");
 		String pass = request.getParameter("pass");
 
+		/* ADD @author 近藤 */
+		if (!pass.contentEquals(request.getParameter("pass")))
+		{	// 確認欄とパスワードが一致しない場合は登録処理を行わずlogin.jspへ
+
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+			dispatcher.forward(request, response);
+
+		}	//if end
+
+		//nameとpassの値をaccounBeansに保存。
+		AccountBeans account = new AccountBeans();
+		account.setName(name);
+		account.setPass(pass);
+		HttpSession session = request.getSession();
+		session.setAttribute("accounBeans",account);
+
+		// セッションスコープへ処理内容を送信
+		String action = "post";
+		session.setAttribute("action", action);
+
 		//AccountDAO生成
 		AccountRegistDAO accountRegistDAO = new AccountRegistDAO();
-		AccountBeans accounBeans = new AccountBeans();
-		accounBeans.setName(name);
-		accounBeans.setPass(pass);
+
 		//既存アカウント名と被らないように新規アカウントを保存。
-		boolean check = accountRegistDAO.createAccount(accounBeans);
-System.out.println(check);
+		boolean check = accountRegistDAO.createAccount(account);
+
 		if(check) {
 
-			//被らないので、新規登録。AccountBeansnに保存。
-
-			HttpSession session = request.getSession();
-			session.setAttribute("accounBeans",accounBeans);
-
-			//新規アカウント登録後、ログイン完了画面へ
-			RequestDispatcher dispatcher =
-					request.getRequestDispatcher("/loginResult.jsp");
+			//新規アカウント登録&ログイン完了
+			// loginResult.jspへ
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/loginResult.jsp");
 			dispatcher.forward(request, response);
 
 		}else {
 
-			//被るので登録出来ず、リダイレクト。
-			accounBeans = null;
-			RequestDispatcher dispatcher =
-					request.getRequestDispatcher("/login.jsp");
+			//被るので登録不可
+			// セッションスコープとBeansを破棄
+			session.removeAttribute("accounBeans");
+			account = null;
+
+			// login.jspへ
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
 			dispatcher.forward(request, response);
 
 		}
